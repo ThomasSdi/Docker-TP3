cc725560004a0d96a898cdf8ff872aa869760e3a
Thomas <95448420+ThomasSdi@users.noreply.github.com>
2025-01-28 19:48:10 +0100

    Update test.php

--- etape2/src/test.php
+++ etape2/src/test.php
@@ -1,20 +1,21 @@
 <?php
-    $mysqli = new mysqli('bdd', 'monuser', 'password');
+    $mysqli = new mysqli('data', 'monuser', 'password', 'mabase');
     if (mysqli_connect_errno()) {
         printf("Connect failed: %s\n", mysqli_connect_error());
         exit();
     }
 
-	if ($mysqli->query("INSERT INTO mabase.matable (compteur) SELECT count(*)+1 FROM mabase.matable;") === TRUE) {
-	    printf("Count updated\n<br />", $result->num_rows);
-	}
+    if ($mysqli->query("INSERT INTO mabase.matable (compteur) SELECT count(*)+1 FROM mabase.matable;") === TRUE) {
+        printf("Count updated\n<br />");
+    }
 
-	if ($result = $mysqli->query("SELECT * FROM mabase.matable")) {
-	    printf("Count : %d\n<br />", $result->num_rows);
+    if ($result = $mysqli->query("SELECT * FROM mabase.matable")) {
+        printf("Count : %d\n<br />", $result->num_rows);
 
-	    /* Libération du jeu de résultats */
-	    $result->close();
-	}
+        $result->close();
+    } else {
+        printf("Error with SELECT query: %s\n", $mysqli->error);
+    }
 
     $mysqli->close();
 ?>
